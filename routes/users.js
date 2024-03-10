2e91a38f1270aa986e1770e04089a54a62f81734
YechanRyu <yechanryu9803@gmail.com>
2024-03-10 16:17:54 -0500

    user.js for product

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var product =1;
+var factor =1;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  factor +=1;
+  product *= factor;
+  res.send(`Product is: ${product}`);
 });
 
 module.exports = router;
