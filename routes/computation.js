cf1a624e2555d4f6fdf38bb1e2c288a4501bb976
YechanRyu <yechanryu9803@gmail.com>
2024-03-10 16:50:49 -0500

    bonus question edition

--- routes/computation.js
+++ routes/computation.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var x = Math.random();
-var y = Math.random();
+var x = Math.floor(Math.random());
+var y = Math.floor(Math.random());
 var r = Math.hypot(x,y);
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-  res.send(`Math.hypot${x,y} applied to ${x,y} is ${r}`);
+  res.send(`Math.hypot(${x,y}) applied to ${x,y} is ${r}`);
 });
 
 module.exports = router;
