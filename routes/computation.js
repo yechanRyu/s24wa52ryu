6c042cef7e83415d24e3cbb4754145aacad40952
YechanRyu <yechanryu9803@gmail.com>
2024-03-10 16:54:21 -0500

    bonus edition 2

--- routes/computation.js
+++ routes/computation.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var x = Math.floor(Math.random());
-var y = Math.floor(Math.random());
+var min = 1;
+var max = 10;
+var x = Math.floor(Math.random()* (max - min + 1) + min);
+var y = Math.floor(Math.random()* (max - min + 1) + min);
 var r = Math.hypot(x,y);
 /* GET users listing. */
 router.get('/', function(req, res, next) {
